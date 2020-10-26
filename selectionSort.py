f22f448e36945f2ff55cac9ab304ce06ccdd86e3
mo cox <mo_cox@msn.com>
2020-10-26 12:16:16 +0000

    added selection sort

---
+++ selectionSort.py
@@ -0,0 +1,33 @@
+## Selection Sorting
+## Big O
+## Best     Average     Worst
+## O(n^2)   O(n^2)      O(n^2)
+## Since we are lopping within a loop, it is n^2
+
+def swap(arr, index1, index2):
+    temp = arr[index1]
+    arr[index1] = arr[index2]
+    arr[index2] = temp
+
+    return arr
+    
+
+def selection_sort(arr):
+    ## outer loop
+    for i in range(0, len(arr) - 1, 1):
+        lowest = i
+        ## inner loop
+        for j in range(i + 1, len(arr), 1):
+            if arr[lowest] > arr[j]:
+                lowest = j
+        ## check if we need to swap
+        if i != lowest: 
+            swap(arr, i, lowest)
+    
+    return arr
+
+
+
+arr = selection_sort([0,2,34,22,10,18,13, 1])
+print("Sorted: -> ", arr)
+
