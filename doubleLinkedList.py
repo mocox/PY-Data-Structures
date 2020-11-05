b178c622e9a954ba031823f32032ddd838691deb
Vlad756 <64403999+Vlad756@users.noreply.github.com>
2020-11-05 15:14:23 +0300

    added comments

--- doubleLinkedList.py
+++ doubleLinkedList.py
@@ -5,8 +5,6 @@
 
 
 # A single node of a double linked list
-
-
 class Node():
     # constructor
     def __init__(self, data):
@@ -22,6 +20,7 @@ class LinkedList():
         self.tail = None
         self.length = 0
 
+    # add element to the end of the list
     def append(self, data):
         new_node = Node(data)
         new_node.prev = self.tail
@@ -34,6 +33,7 @@ class LinkedList():
             self.tail = new_node
             self.length += 1
 
+    # add element at the begining of the list
     def prepend(self, data):
         new_node = Node(data)
         new_node.next = self.head
@@ -41,6 +41,7 @@ class LinkedList():
         self.head = new_node
         self.length += 1
 
+    # add element at the given position of the list
     def insert(self, index, data):
         if index >= self.length:
             self.append(data)
@@ -53,6 +54,7 @@ class LinkedList():
         follower.prev = new_node
         self.length += 1
 
+    # traverse to index
     def traverseToIndex(self, index):
         counter = 0
         currentNode = self.head
@@ -61,6 +63,7 @@ class LinkedList():
             counter += 1
         return currentNode
 
+    # remove element at the specified position
     def remove(self, index):
         if index >= self.length:
             print('Exception: Index Out Of Range!')
@@ -69,6 +72,7 @@ class LinkedList():
         leader.next = unwantedNode.next
         self.length -= 1
 
+    # reverse the list
     def reverse(self):
         if not self.head.next:
             return self.head
@@ -83,12 +87,15 @@ class LinkedList():
         self.head.next = None
         self.head = first
 
+    # get length of the list
     def getLength(self):
         return self.length
 
+    # get element at the given position
     def get(self, index):
         return self.traverseToIndex(index).data
 
+    # set element at the given position
     def set(self, index, data):
         first = self.head
         counter = 0
@@ -100,6 +107,7 @@ class LinkedList():
         self.head = first
         return temp
 
+    # print list
     def printl(self):
         temp = self.head
         while temp != None:
